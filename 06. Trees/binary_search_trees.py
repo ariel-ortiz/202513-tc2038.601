3fc49dc9b4551cfdee9716be8d071d057adee85d
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-09-30 10:51:31 -0600

    Add binary search tree traversal functions: in-order, pre-order, post-order, and level-order

---
+++ 06. Trees/binary_search_trees.py
@@ -0,0 +1,58 @@
+from typing import Any, Iterator
+from collections import deque
+
+
+type Tree = list[Any] | None
+
+
+def in_order(root: Tree) -> Iterator[Any]:
+    if root is None:
+        return
+    yield from in_order(root[1])
+    yield root[0]
+    yield from in_order(root[2])
+
+
+def pre_order(root: Tree) -> Iterator[Any]:
+    if root is None:
+        return
+    yield root[0]
+    yield from pre_order(root[1])
+    yield from pre_order(root[2])
+
+
+def post_order(root: Tree) -> Iterator[Any]:
+    if root is None:
+        return
+    yield from post_order(root[1])
+    yield from post_order(root[2])
+    yield root[0]
+
+
+def level_order(root: Tree) -> Iterator[Any]:
+    queue: deque[Tree] = deque()
+    queue.append(root)
+    while queue:
+        current: Tree = queue.popleft()
+        if current is not None:
+            value, left, right = current
+            yield value
+            queue.append(left)
+            queue.append(right)
+
+
+
+if __name__ == "__main__":
+    t: Tree = [5,
+               [3,
+                [1, None, None],
+                [4, None, None]],
+               [8,
+                None,
+                [10, 
+                 [9, None, None],
+                 None]]]
+    print(list(in_order(t)))
+    print(list(pre_order(t)))
+    print(list(post_order(t)))
+    print(list(level_order(t)))
