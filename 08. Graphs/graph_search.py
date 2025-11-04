d49e846320119a8a8d2e971999824c911934f576
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-11-04 10:56:39 -0600

    Add depth-first search implementation for graph traversal

---
+++ 08. Graphs/graph_search.py
@@ -0,0 +1,40 @@
+from typing import Iterator
+from collections import deque
+
+
+type Graph = dict[str, list[str]]
+
+
+def depth_first_search(
+        start: str,
+        graph: Graph) -> Iterator[str]:
+    stack: deque[str] = deque()
+    visited: set[str] = set()
+    stack.append(start)
+    while stack:
+        current: str = stack.pop()
+        if current not in visited:
+            visited.add(current)
+            stack.extend(graph[current][::-1])
+            yield current
+
+
+if __name__ == "__main__":
+    g: Graph = {
+        'A': ['B', 'C', 'F'],
+        'B': ['A', 'D'],
+        'C': ['A', 'E', 'F'],
+        'D': ['B'],
+        'E': ['C', 'F'],
+        'F': ['A', 'C', 'E', 'G'],
+        'G': ['F']
+    }
+    print(list(depth_first_search('A', g)))
+    print(list(depth_first_search('G', g)))
+    print(list(depth_first_search('E', g)))
+    start = 'A'
+    end = 'D'
+    for v in depth_first_search(start, g):
+        print(v)
+        if v == end:
+            break
